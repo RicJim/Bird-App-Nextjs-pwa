194c5979d80ac79028a212376f90325b1433a2f9
Atychis <ricardojimenez00759@gmail.com>
2024-11-18 00:50:29 -0500

    Fastapi Config

--- src/components/identify/AudioClassifier.jsx
+++ src/components/identify/AudioClassifier.jsx
@@ -26,7 +26,7 @@ export default function AudioClassifier() {
 
       try {
         const response = await fetch(
-          "http://127.0.0.1:8000/api/py/predict_sound",
+          "https://bird-app-nextjs-fastapi.vercel.app/api/py/predict_sound",
           {
             method: "POST",
             body: formData,
