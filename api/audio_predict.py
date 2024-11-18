c82889fe4d09f63f6f950eab921a618ffdacd4b5
Atychis <ricardojimenez00759@gmail.com>
2024-11-18 01:14:46 -0500

    requirements upgrade

--- api/audio_predict.py
+++ api/audio_predict.py
@@ -1,7 +1,7 @@
 import os
-import librosa
+# import librosa
 from io import BytesIO
-import numpy as np
+# import numpy as np
 
 def mel_spectrogram(file):
     try:
@@ -10,8 +10,8 @@ def mel_spectrogram(file):
         audio_file = BytesIO(audio_data)
 
         # Cargar el archivo usando librosa
-        y, sr = librosa.load(audio_file, sr=None)
-        mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
+        # y, sr = librosa.load(audio_file, sr=None)
+        # mel_spectrogram = librosa.feature.melspectrogram(y=y, sr=sr)
 
         print("Entró en mel_spectrogram y procesó el archivo")  # Mensaje de verificación
         return "Tarea mel_spectrogram completada"
